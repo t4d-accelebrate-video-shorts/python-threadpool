52cbeb1fc764a7247366dc4ddcb2585ddfa2e69f
Eric Greene <eric@t4d.io>
2021-05-09 08:57:12 -0400

    create cookie cutter project

---
+++ start/python_threadpool/__main__.py
@@ -0,0 +1,14 @@
+"""Main Module"""
+
+from python_threadpool.some_pkg.some_mod import do_it
+
+
+def main() -> None:
+    """Main Function"""
+
+    do_it()
+    print("Welcome to Package python_threadpool")
+
+
+if __name__ == '__main__':
+    main()
